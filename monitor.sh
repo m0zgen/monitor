67e1535054d37664e8546407dc50d983b12264c8
/:) <g.yevgeniy.p@gmail.com>
2022-06-07 16:26:36 +0200

    + checking func arg

--- monitor.sh
+++ monitor.sh
@@ -10,40 +10,51 @@ SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 # Initial variables
 # ---------------------------------------------------\
 
-# Script has argument or not
-_SVC=$@
-# Def status
 _STAT=0
-_ACTION=$2
 
-# Functions
+# Init
 # ---------------------------------------------------\
 
 # Help
 usage() {
-    echo -e "Pass service name as argument please"
+    echo -e "Pass service name as argument please:
+    -u - unit name
+    -a - path to bash action script
+    -
+    "
     exit 1
 }
 
+# Argument passing
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    usage
+fi
+
+# Checks arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -u|--unit)      _UNIT_NAME=1; _SERVICE="$2"; shift;;
+        -a|--action)    _ACTION=1 _SCRIPT="$2"; shift;;
+        -i|--is)    _IS=1 _IS_BOOL="$2"; shift;;
+        -h|--help) usage ;; 
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Functions
+# ---------------------------------------------------\
+
 # Checking active status from systemd unit
 checkSVC() {
+    _SVC=$1
     systemctl is-active $_SVC >/dev/null 2>&1 && _STAT=1 # || _STAT=0
 }
 
-# Action
-checkSTAT() {
+runAction() {
 
-    checkSVC
-
-    if [[ "$_STAT" -eq "1" ]]; then
-    
-        echo -e "Service: $_SVC still running"
-        
-    else
-
-        echo -e "Service: $_SVC has stopped status"
-
-        if [[ -n "$_ACTION" ]]; then
+    if [[ -n "$_ACTION" ]]; then
             echo -e "Try to run action script..."
 
             if [[ -f  "$_ACTION" ]]; then
@@ -54,6 +65,20 @@ checkSTAT() {
             fi
 
         fi
+}
+
+# Action
+checkSTAT() {
+
+    checkSVC $1
+
+    if [[ "$_STAT" -eq "1" ]]; then
+    
+        echo -e "Service: $_SVC still running"
+        
+    else
+
+        echo -e "Service: $_SVC has stopped status"
 
     fi
 }
@@ -61,10 +86,11 @@ checkSTAT() {
 # Runner
 # ---------------------------------------------------\
 
-# Argument passing
-if [ $# -eq 0 ]; then
-    echo "No arguments provided"
-    usage
-else
-    checkSTAT
+if [[ "$_UNIT_NAME" -eq "1" ]]; then
+
+    if [[ -z "$_SERVICE" ]]; then
+        echo "Please set unit.service name in -u."
+    else
+        checkSTAT $_SERVICE
+    fi
 fi
