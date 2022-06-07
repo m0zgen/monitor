515b994a2491d14cbf279e47bfd3c6e9754e40ef
/:) <g.yevgeniy.p@gmail.com>
2022-06-07 16:51:31 +0200

    + checking unit exists and recovery

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Monitor.sh - Script for checking systemd unit status
 
 And `monitor.sh` can run custom script / action if unit has stopped or running statuses
+
+You can use multiple argument for checking and actions depends from result `is-astatus` status:
+
+```bash
+./monitor.sh -u sshd -a "/path/to/action-script/action.sh"
+````
--- monitor.sh
+++ monitor.sh
@@ -37,6 +37,7 @@ while [[ "$#" -gt 0 ]]; do
         -u|--unit)      _UNIT_NAME=1; _SERVICE="$2"; shift;;
         -a|--action)    _ACTION=1 _SCRIPT="$2"; shift;;
         -i|--is)    _IS=1 _IS_BOOL="$2"; shift;;
+        -r|--recover) _RECOVER=1 ;;
         -h|--help) usage ;; 
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
@@ -48,7 +49,7 @@ done
 
 # Checking active status from systemd unit
 checkSVC() {
-    _SVC=$1
+    local _SVC=$1
     systemctl is-active $_SVC >/dev/null 2>&1 && _STAT=1 # || _STAT=0
 }
 
@@ -67,6 +68,32 @@ runAction() {
         fi
 }
 
+checkSCRIPT() {
+    if [[ ! -f $1 ]]; then
+        echo -e "Action starter script does not found."
+    else
+        bash $1
+    fi
+}
+
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+service_enabled() {
+    local n=$1
+    if [[ `systemctl is-enabled "$n"` ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # Action
 checkSTAT() {
 
@@ -78,7 +105,32 @@ checkSTAT() {
         
     else
 
-        echo -e "Service: $_SVC has stopped status"
+        if service_exists $_SVC; then
+
+            echo -e "Service: $_SVC has stopped status"
+
+            if [[ "$_ACTION" -eq "1" ]]; then
+
+                if [[ -z "$_SCRIPT" ]]; then
+                    echo "Please set -a action runner script path."
+                else
+                    checkSCRIPT $_SCRIPT
+                fi
+            fi 
+
+            if [[ "$_RECOVER" -eq 1 ]]; then
+
+                if service_exists $_SVC; then
+                systemctl enable --now $_SVC
+
+                fi
+                systemctl restart $_SVC
+            fi
+        else
+            echo -e "Systemd unit $_SVC does not exist"
+        fi
+
+        
 
     fi
 }
